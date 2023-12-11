e635fb13aef5b73e80996fe4c58f5f3175e3d0e2
szdxdxdx <3510745357@qq.com>
2023-12-11 14:30:55 +0800

    小改

--- src/pages/user/LoginPage.vue
+++ src/pages/user/LoginPage.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- 原神背景视频 -->
-  <div style="width: 100%; position: fixed; z-index: -1;">
+  <div style="width: 100%; height: 100%; position: fixed; z-index: -1;">
     <video src="https://www.yuanshen.com/medias/bg.3e78e808.mp4"
         preload="auto"
         poster="images/poster.948ee40e.jpg"
@@ -16,7 +16,7 @@
       <!-- 登录的form表单 -->
       <div class="login-form">
 
-        <h1 class="login-form-item title">
+        <h1 class="login-form-item login-form-title">
           icebee 高校教务管理系统
         </h1>
 
@@ -41,9 +41,13 @@
           登录
         </a-button>
 
-        <a-link class="forgot-passwd-link"
-          :hoverable="false" icon>
+        <a-link class="link" :hoverable="false" icon>
           忘记密码 / 账号申诉
+        </a-link>
+
+        <a-link class="link" :hoverable="false" icon style="float: right;"
+          href="https://ys.mihoyo.com/main/">
+          官网
         </a-link>
       </div>
 
@@ -67,6 +71,9 @@ export default {
     }
   },
   methods: {
+    clear_passwd() {
+      this.passwd = ""
+    },
     set_login_err_msg(msg) {
       this.login_err_msg = msg
     },
@@ -93,32 +100,41 @@ export default {
       })
       .then(resp => {
         this.set_is_login_btn_loading(false)
-        
+
         if (resp.status_code != 200) {
           this.set_login_err_msg(resp.msg)
+          this.clear_passwd()
           return
         }
 
-        console.log(resp)
+        let query_data = {
+          user_id: resp.data.user_id,
+          role: resp.data.role
+        }
+
         if (resp.data.role === "student") {
-          this.$router.push({ path: '/student/home', query: { user_id: resp.data.user_id } })
+          this.$router.push({ path: '/student/home', query: query_data })
         }
         else if (resp.data.role === "teacher") {
-          this.$router.push({ path: '/teacher/home', query: { user_id: resp.data.user_id } })
+          this.$router.push({ path: '/teacher/home', query: query_data })
         }
         else if (resp.data.role === "admin") {
-          this.$router.push({ path: '/admin/home', query: { user_id: resp.data.user_id } })
+          this.$router.push({ path: '/admin/home', query: query_data })
         }
         else {
-          /* assert false */
           this.set_login_err_msg("无法登录，未知的用户身份")
-          return
         }
       })
       .catch(err => {
         this.set_is_login_btn_loading(false)
-        this.set_login_err_msg(err.message)
-        return
+        this.clear_passwd()
+
+        if (err.code === "ERR_NETWORK") {
+          this.set_login_err_msg("网络错误")
+        }
+        else {
+          this.set_login_err_msg(err.message)
+        }
       })
     }
   }
@@ -133,16 +149,17 @@ export default {
   background-color: rgba(255, 255, 255, 0.85);
   border-radius: var(--border-radius-small);
 }
-.login-form-item {
-  margin-top: 9px;
-  margin-bottom: 9px;
-}
 
-.title {
+.login-form-title {
   text-align: center;
   font-size: 22px;
   font-weight: 600;
   color: var(--color-text-1);
+}
+
+.login-form-item {
+  margin-top: 9px;
+  margin-bottom: 9px;
 }
 
 .login-form-input {
@@ -157,10 +174,13 @@ export default {
   font-size: 13px;
 }
 
-.forgot-passwd-link {
-  padding-left: 2px;
+.link {
   color: var(--color-text-3);
   font-size: 13px;
+}
+
+.link:hover {
+  color: rgb(var(--link-5));
 }
 
 </style>
